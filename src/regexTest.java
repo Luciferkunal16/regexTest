81a42c278b8e5d74f7f97f4b3fed60603bd98c44
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-13 16:03:05 +0530

    Added UC3: Email With Specified Format

--- src/regexTest.java
+++ src/regexTest.java
@@ -3,9 +3,13 @@ public class regexTest {
     public static void main(String[] args) {
         String firstName="Kunal";
         String lastName="Batham";
-        String regex="^[A-Z]{1}[a-z]{2}[a-z]*";
+        String email="abc.xyx@bl.co.in";
+        String regexName="^[A-Z]{1}[a-z]{2}[a-z]*";
+        String regexEmail="[a-zA-Z]+[a-zA-Z0-9]*[.]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
 
-        System.out.println(firstName.matches(regex));
-        System.out.println(lastName.matches(regex));
+
+        System.out.println(firstName.matches(regexName));
+        System.out.println(lastName.matches(regexName));
+        System.out.println(email.matches(regexEmail));
     }
 }

381889ac43cc6cfbd91526a113246c9f5dc36625
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-13 15:56:12 +0530

    Added UC2: fisr letter start with Capital letter and must have 3 alphabets

--- src/regexTest.java
+++ src/regexTest.java
@@ -2,7 +2,10 @@ import java.util.regex.*;
 public class regexTest {
     public static void main(String[] args) {
         String firstName="Kunal";
+        String lastName="Batham";
         String regex="^[A-Z]{1}[a-z]{2}[a-z]*";
+
         System.out.println(firstName.matches(regex));
+        System.out.println(lastName.matches(regex));
     }
 }
